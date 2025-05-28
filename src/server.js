f19ca3049876261602b8a3ac17e75c7a4b8956b7
dheerajdamala <d.dheeraj.12120@gmail.com>
2025-05-28 15:24:38 +0530

    cron.js added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 //const express = require('express');
 import express from 'express';
 import dotenv from 'dotenv';
-import {initDB} from './config/db.js'; // Adjust the path as necessary
+import {initDB} from './config/db.js';
+import job from './config/cron.js'; // Adjust the path as necessary
 import rateLimiter from './middleware/rateLimiter.js';
 import transactionRoute from './routes/transactionroutes.js'; // Adjust the path as necessary
 dotenv.config();
@@ -9,6 +10,9 @@ dotenv.config();
 
 const app = express();
 
+if (process.env.NODE_ENV === 'production') {
+job.start(); // Start the cron job if in production
+}
 
 //midleware
 app.use(rateLimiter);
@@ -16,7 +20,10 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/api/health', (req, res) => {
 
+  res.status(200).json({status: 'ok' });
+});
 
 app.use("/api/transactions", transactionRoute);
 
